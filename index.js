5a48c3729f3661edd39ea7afba43b23bb5032701
wmuza <wmuza@meticulosity.co.za>
2018-08-12 08:05:50 +0200

    Adding Aria Roles to Restaurant.html and registering the service worker

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const app = express();
 const http = require('http').Server(app);
 const fs = require('fs');
 
-app.use('/img', express.static('img'))
-app.use('/images_src', express.static('images_src'))
-app.use('/js', express.static('js'))
-app.use('/css', express.static('css'))
-app.use('/data', express.static('data'))
+app.use('/img', express.static('img'));
+app.use('/images_src', express.static('images_src'));
+app.use('/js', express.static('js'));
+app.use('/css', express.static('css'));
+app.use('/data', express.static('data'));
 
 app.get('/', (req, res) =>  {
   res.sendFile(__dirname + '/');  
@@ -19,6 +19,9 @@ app.get('/restaurant.html', (req, res) =>  {
   res.sendFile(__dirname + '/restaurant.html');  
 });
 
+app.get('/sw.js', (req, res) =>  {
+  res.sendFile(__dirname + '/sw.js');  
+});
 
 http.listen(80, () => console.log('listening on *:80'));
     
