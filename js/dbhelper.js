561d4c8be703c8626ada837128f161cac549bdfa
wmuza <wmuza@meticulosity.co.za>
2018-08-12 10:18:39 +0200

    changing host url

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,8 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 80 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+	const host = window.location.href; // getting the host url
+    return `${host}:${port}/data/restaurants.json`;
   }
 
   /**
