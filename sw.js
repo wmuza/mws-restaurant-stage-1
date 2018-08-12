70be9233b522d0af1def5aeaf150c9c6f0ceb043
wmuza <wmuza@meticulosity.co.za>
2018-08-12 10:03:09 +0200

    caching page assets and Intercepting web page requests
    caching page assets and Intercepting web page requests
    
    I also added a fall back image to be displayed if there is a broken image

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const swCacheName = 'static-restaurant-01';
+const swCacheName = 'static-restaurant-02';
 
 // list of assets to cache on install
 // cache each restaurant detail page as well
@@ -8,12 +8,12 @@ self.addEventListener('install', event => {
       .then(cache => {
         return cache.addAll([
           '/index.html',
+          // '/restaurant.html',
           '/css/styles.css',
           '/js/dbhelper.js',
           '/js/register_sw.js',
           '/js/main.js',
           '/js/restaurant_info.js',
-          // '/data/restaurants.json',
           '/restaurant.html?id=1',
           '/restaurant.html?id=2',
           '/restaurant.html?id=3',
@@ -24,10 +24,50 @@ self.addEventListener('install', event => {
           '/restaurant.html?id=8',
           '/restaurant.html?id=9',
           '/restaurant.html?id=10',
-          '/img/fixed/offline_img1.png'
+          '/img/offline.png'
+          
         ]).catch(error => {
           console.log('Caches open failed: ' + error);
         });
       })
+  );
+});
+
+// intercept all requests
+// either return cached asset or fetch from network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    // Add cache.put to cache images on each fetch
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).then(fetchResponse => {
+        return caches.open(swCacheName).then(cache => {
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      });
+    }).catch(error => {
+      if (event.request.url.includes('.jpg')) {
+        return caches.match('/img/offline.png');
+      }
+      return new Response('Not connected to the internet', {
+        status: 404,
+        statusText: "Not connected to the internet"
+      });
+    })
+  );
+});
+
+// delete old/unused static caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          return cacheName.startsWith('static-restaurant-') && cacheName !== swCacheName;
+        }).map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
   );
 });
